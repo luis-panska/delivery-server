41e6f7d9940d9298e57be6ac21c2aee9278e1980
Jeancarlo Mamani Lingan <jeancarlo@latamready.com>
2022-06-11 15:49:12 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./db/connection");
 
@@ -7,6 +8,7 @@ const app = express();
 
 dotenv.config();
 
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +19,7 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/user", require("./routes/user.routes"));
+app.use("/api/users", require("./routes/user.routes"));
 
 connectDB().then(() => {
   app.listen(process.env.PORT || 4500, () => {
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,20 @@ const router = express.Router();
 
 // Login
 router.post("/login", (req, res) => {
-  res.send("Hello World");
+  try {
+    const { email, password } = req.body;
+    console.log(email, password);
+    return res.status(200).json({
+      ok: true,
+      message: "Login successful",
+      token: "token",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      message: "Login failed",
+    });
+  }
 });
 
 module.exports = router;
