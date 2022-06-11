e4425b3535f6d88afbedb827f098dd9edc1edf67
Jeancarlo Mamani Lingan <jeancarlo@latamready.com>
2022-06-11 16:59:44 -0500

    validate data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,24 +2,31 @@ const bycrypt = require("bcryptjs");
 const User = require("../models/user.model");
 
 const validateEmailInUse = async (email) => {
+  if (!email) throw new Error("Email is required");
   const user = await User.findOne({ email });
   if (user) throw new Error("Email already in use");
 };
 
 const validatePhoneInUse = async (phone) => {
+  if (!phone) throw new Error("Phone is required");
   const user = await User.findOne({ phone });
   if (user) throw new Error("Phone already in use");
 };
 
 const validateUsername = async (username) => {
+  if (!username) throw new Error("Username is required");
   const user = await User.findOne({ username });
   if (user) throw new Error("Username already in use");
 };
 
-const create = async (username, phone, email, password) => {
+const validateData = async () => {
   await validateEmailInUse(email);
   await validateUsername(username);
   await validatePhoneInUse(phone);
+};
+
+const create = async (username, phone, email, password) => {
+  await validateData();
   const newUser = new User({
     username,
     email,
