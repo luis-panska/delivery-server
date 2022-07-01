c25029e1146684c41def12badbffa1c2f8625a3b
Luis Mamani Lingán <72869012@certus.edu.pe>
2022-07-01 16:07:08 -0500

    jwt fix2

--- src/middlewares/token.handler.js
+++ src/middlewares/token.handler.js
@@ -5,8 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const validateJWT = (req, res, next) => {
   try {
-    const token = req.headers["Authorization"];
-    console.log(token);
+    const token = req.headers.authorization.split(" ")[1];
     if (!token) {
       return res.status(401).json({
         ok: false,
